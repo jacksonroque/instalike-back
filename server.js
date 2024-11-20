b0783fd433f8a16301b1abd8108440fc4459f82e
Jackson Silva <jackson.roque@protonmail.com>
2024-11-20 20:13:07 -0300

    Ajustes de configuração do banco de dados

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+import conectarAoBanco from "./src/config/dbConfig.js";
+
+await conectarAoBanco(process.env.STRING_CONEXAO)
+
+const posts = [
+    { id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150" },
+    { id: 2, descricao: "Gato brincando com um novelo de lã", imagem: "https://placecats.com/millie/300/150", },
+    { id: 2, descricao: "Gato fazendo panqueca", imagem: "https://placecats.com/millie/300/150", },
+];
+
+
+
 app.listen(3000, () => {
-    console.log("Servidor escutando...");
+    console.log("servidor escutando...");
 });
 
-app.get("/api",(req, res)=>{
-    res.status(200).send("A Torre Eifiel iluminada à noite, com milhares de luzes cintilando, criando um espetáculo mágico em Paris.");
-});
+function buscarPostPorID(id){
+    return posts.findIndex((post) => {
+        return post.id === Number(id);
+    })
+}
+
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorID(req.params.id)
+    res.status(200).json(posts[index]);
+})
